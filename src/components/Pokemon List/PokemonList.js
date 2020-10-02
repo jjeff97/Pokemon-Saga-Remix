0cd5b5dc0b3a0f0012b0f50d3aeccdb7ab632e6b
James Jeffeson <jrjefferson830@gmail.com>
2020-10-01 21:20:34 -0500

    image component set up

--- src/components/Pokemon List/PokemonList.js
+++ src/components/Pokemon List/PokemonList.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import PokemonListItem from '../PokemonListItem/PokemonListItem';
+import './PokemonList.css';
 class PokemonList extends Component {
   render() {
     const pokemonListHTMLArray = this.props.store.pokemonReducer.map(
       (item, index) => {
         return (
-          <div>
-            <img src={item.images} />
-            <h4>{item.name}</h4>
-
-          </div>
+          <PokemonListItem pokemon={item} />
         );
       }
     );
