b2879d479a34ed7cfa8e5e0d93a4861d3b701b8d
James Jeffeson <jrjefferson830@gmail.com>
2020-10-04 19:22:12 -0500

    get error in pokemon reducer

--- server/routes/pokemon.router.js
+++ server/routes/pokemon.router.js
@@ -1,40 +1,41 @@
-const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
 //get all pokemon
 router.get('/', (req, res) => {
-    const queryString = `SELECT * FROM pokemon`;
+  const queryString = `SELECT * FROM pokemon`;
 
-    pool.query(queryString)
+  pool
+    .query(queryString)
     .then((response) => {
-        res.send(response.rows);
-    }).catch((err) => {
-        console.log(err);
-        res.sendStatus(500);
+      res.send(response.rows);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.sendStatus(500);
     });
 });
 
 //get all types
-router.get ('/full', (req, res) => {
-    const queryString = `SELECT pokemon.id, pokemon.name, pokemon.description, pokemon.images, array_agg(type.description) as types FROM pokemon
+router.get('/full', (req, res) => {
+  const queryString = `SELECT pokemon.id, pokemon.name, pokemon.description, pokemon.images, array_agg(type.description) as types FROM pokemon
     JOIN pokemon_type ON pokemon.id=pokemon_type.pokemon_id
         JOIN type ON pokemon_type.type_id=type.id
     GROUP BY pokemon.id
-    ORDER BY pokemon.id;`
+    ORDER BY pokemon.id;`;
 
-    pool.query(queryString)
+  pool
+    .query(queryString)
     .then((response) => {
-        res.send(response.rows);
+      res.send(response.rows);
     })
     .catch((err) => {
-        console.log(err);
-        res.sendStatus(500);
-    })
-})
+      console.log(err);
+      res.sendStatus(500);
+    });
+});
 
 //get pokemon with types
-
 
 module.exports = router;
