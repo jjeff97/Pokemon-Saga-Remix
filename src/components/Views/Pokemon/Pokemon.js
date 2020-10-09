db34d5924fdd609b90942aaa3c393e8bd097764b
James Jeffeson <jrjefferson830@gmail.com>
2020-10-08 21:07:43 -0500

    Profile page setup

--- src/components/Views/Pokemon/Pokemon.js
+++ src/components/Views/Pokemon/Pokemon.js
@@ -1,40 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './Pokemon.css';
 
 class Pokemon extends Component {
-
-    state = {
-        pokemon: {}
-    }
-
-    componentDidMount() {
-        console.log(this.props.match.params.id);
-
-let currentId = this.props.match.params.id;
-        let currentPokemon = {};
-    
-    for(let pokemon of this.props.store.pokemonReducer) {
-        if(currentId === pokemon.id) {
-            currentPokemon = pokemon;
-        }
-    }
-this.setState(
-    {
-        pokemon: currentPokemon,
+  state = {
+    pokemon: {
+      types: [],
     },
-    () => {
-        console.log(this.state.pokemon)
+  };
+
+  componentDidMount() {
+    console.log(this.props.match.params.id);
+
+    let currentId = this.props.match.params.id;
+    let currentPokemon = {};
+
+    for (let pokemon of this.props.store.pokemonReducer) {
+      if (currentId == pokemon.id) {
+        currentPokemon = pokemon;
+      }
     }
-    
-    )
-}
+    this.setState(
+      {
+        pokemon: currentPokemon,
+      },
+      () => {
+        console.log(this.state.pokemon);
+      }
+    );
+  }
+backClicked = (event => {
+    this.props.history.push('/');
+})
+  render() {
+    const typesArray = this.state.pokemon.types
+      ? this.state.pokemon.types.map((item, index) => {
+          return <span>{item.toUpperCase()}</span>;
+        })
+      : [];
 
-    render() {
-        return( <div>
-
+    return (
+      <div className="currentPokemon">
+          <div>
+        <button onClick={this.backClicked}>BACK</button>
         </div>
-        )
-    }
+        <img src={this.state.pokemon.images} />
+        <h1>{this.state.pokemon.name}</h1>
+        <h6>{typesArray}</h6>
+        <p>{this.state.pokemon.description}</p>
+      </div>
+    );
+  }
 }
 
 const mapStoreToProps = (store) => ({ store });
